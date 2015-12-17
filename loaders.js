0e58d6425362d70d1098fd4ff653053849daf961
Eamon Hyland <ehyland90@gmail.com>
2015-12-17 21:49:45 +0000

    Updated loader test for ttf|eot|svg files

--- loaders.js
+++ loaders.js
@@ -16,7 +16,7 @@ module.exports = [
     test: /\.woff(\?v=[0-9].[0-9].[0-9])?$/,
     loader: "url?limit=10000&mimetype=application/font-woff&name=[name].[hash].[ext]"
   },{
-    test: /\.(ttf|eot|svg)$/,
+    test: /\.(ttf|eot|svg)(\?v=[0-9].[0-9].[0-9])?$/,
     loader: "file?name=[name].[hash].[ext]"
   },
 
