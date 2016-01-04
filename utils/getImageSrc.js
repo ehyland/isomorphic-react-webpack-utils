7c7b89dbf9512c028825ffd161310f9ee3304f76
Eamon Hyland <ehyland90@gmail.com>
2016-01-04 22:45:28 +0000

    Util to path of complied image on both server and client

---
+++ utils/getImageSrc.js
@@ -0,0 +1,23 @@
+/*
+  Get a complied image from src/images
+*/
+
+function getImageSrc(filename) {
+  if (process.env.BROWSER) {
+    const manifest = require("../../src/images/manifest");
+    return manifest[filename];
+  }else{
+    console.log('Looking for image on server');
+    const searchPath = `./src/images/${filename}`;
+    const stats = require("./getStats")();
+    if (stats && stats.images) {
+      const image = stats.images.find(img => img.original === searchPath);
+      console.log(searchPath);
+      if (image) {
+        return image.compiled;
+      }
+    }
+  }
+}
+
+module.exports = getImageSrc;
