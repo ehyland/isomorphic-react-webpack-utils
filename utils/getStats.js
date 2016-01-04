6e90bacfc25a6ee1f157893e9d726c906cd88182
Eamon Hyland <ehyland90@gmail.com>
2016-01-04 01:21:44 +0000

    Fixed stats path

--- utils/getStats.js
+++ utils/getStats.js
@@ -1,13 +1,15 @@
 const fs = require("fs");
 const path = require("path");
 
+const statsPath = "../../static/build/stats.json";
+
 function getStats () {
   let stats;
   if (process.env.NODE_ENV === "development") {
-    stats = fs.readFileSync(path.resolve(__dirname, "../static/build/stats.json"));
+    stats = fs.readFileSync(path.resolve(__dirname, statsPath));
     stats = JSON.parse(stats);
   }else {
-    stats = require("../static/build/stats.json");
+    stats = require(statsPath);
   }
 
   return stats;
